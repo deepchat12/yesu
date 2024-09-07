19d21851e9174ddc8ae1b473bde9483e5dbc2516
pandi <pandi@gmail.com>
2024-09-07 13:10:59 +0530

    email update

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -24,7 +24,7 @@ export const Contact = (props) => {
     {/* replace below with your own Service ID, Template ID and Public Key from your EmailJS account */ }
     
     emailjs
-      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_PUBLIC_KEY")
+      .sendForm("service_gefjfcj", "template_86j3or7", e.target, "S3EkKgeoM0AxWUW2j")
       .then(
         (result) => {
           console.log(result.text);
